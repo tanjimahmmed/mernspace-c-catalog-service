c94cf911ef4c62dd9e7801640087ab6f52732659
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-11-10 13:08:43 +0600

    add authentication

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -4,13 +4,19 @@ import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../config/logger";
 import { asyncWrapper } from "../common/utils/wrapper";
+import authenticate from "../common/middlewares/authenticate";
 
 const router = express.Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
 
-router.post("/", categoryValidator, asyncWrapper(categoryController.create));
+router.post(
+    "/",
+    authenticate,
+    categoryValidator,
+    asyncWrapper(categoryController.create),
+);
 // router.post("/", categoryValidator, categoryController.create.bind(categoryController));
 
 export default router;
