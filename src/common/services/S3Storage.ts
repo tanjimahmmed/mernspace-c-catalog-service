a8c231a3a1e1f8ddba335b106cac823a54387bff
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-11-17 18:04:27 +0600

    add image url in products collection

--- src/common/services/S3Storage.ts
+++ src/common/services/S3Storage.ts
@@ -5,6 +5,7 @@ import {
 } from "@aws-sdk/client-s3";
 import config from "config";
 import { FileData, FileStorage } from "../types/storage";
+import createHttpError from "http-errors";
 
 export class S3Storage implements FileStorage {
     private client: S3Client;
@@ -39,7 +40,13 @@ export class S3Storage implements FileStorage {
         // @ts-ignore
         return await this.client.send(new DeleteObjectCommand(objectParams));
     }
-    getObjectUri(): string {
-        throw new Error("Method not implemented");
+    getObjectUri(filename: string): string {
+        const bucket = config.get("s3.bucket");
+        const region = config.get("s3.region");
+        if (typeof bucket === "string" && typeof region === "string") {
+            return `http://${bucket}.s3.${region}.amazonaws.com/${filename}`;
+        }
+        const error = createHttpError(500, "Invalid s3 configuration");
+        throw error;
     }
 }
